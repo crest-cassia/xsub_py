f9460dc91099bfc56c96ce5647c1d33d7a4f8091
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-05-19 16:54:32 +0900

    load scheduler from environment variable 'XSUB_TYPE'

--- bin/schedulers/__init__.py
+++ bin/schedulers/__init__.py
@@ -1,6 +1,18 @@
+import os,sys
 from . import none
 
 
-def create(scheduler_name):
-  if scheduler_name == "none":
-    return none.NoneScheduler;
+SCHEDULER_TYPES = {
+  "none": none.NoneScheduler
+}
+
+def create():
+  if not 'XSUB_TYPE' in os.environ:
+    print(f"[Error] Set environment variable 'XSUB_TYPE'")
+    print(f"        available values are {','.join(SCHEDULER_TYPES.keys())}")
+    raise Exception(f"environment variable 'XSUB_TYPE' is not set")
+  xsub_type = os.environ['XSUB_TYPE'].lower()
+  if not xsub_type in SCHEDULER_TYPES:
+    print(f"[Error] invalid scheduler type {xsub_type}", file=sys.stderr)
+    raise Exception(f"scheduler type {xsub_type} is not found")
+  return SCHEDULER_TYPES[xsub_type]
