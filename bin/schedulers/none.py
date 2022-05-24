8587312cd15ad38459488c1b1e67cde3272ec473
Yohsuke Murase <yohsuke.murase@gmail.com>
2022-05-24 15:35:16 +0900

    make it work with Python 3.6

--- bin/schedulers/none.py
+++ bin/schedulers/none.py
@@ -24,7 +24,7 @@ class NoneScheduler:
     log.write(f"{cmd} is invoked\n")
     output = ""
     os.makedirs(work_dir, exist_ok=True)
-    result = subprocess.run(cmd, shell=True, cwd=work_dir, check=True, capture_output=True)
+    result = subprocess.run(cmd, shell=True, cwd=work_dir, check=True, stdout=subprocess.PIPE)
     psid = str(int(result.stdout.splitlines()[-1]))
     log.write(f"process id: {psid}\n")
     return (psid, result.stdout.decode())
@@ -32,7 +32,7 @@ class NoneScheduler:
   @staticmethod
   def all_status() -> str:
     cmd = "ps uxr | head -n 10"
-    result = subprocess.run(cmd, shell=True, check=False, capture_output=True)
+    result = subprocess.run(cmd, shell=True, check=False, stdout=subprocess.PIPE)
     return result.stdout.decode()
 
   @staticmethod
@@ -45,17 +45,17 @@ class NoneScheduler:
   @staticmethod
   def _status(job_id: str) -> Tuple[str,str]:
     cmd = f"ps -p {job_id}"
-    result = subprocess.run(cmd, shell=True, check=False, capture_output=True)
+    result = subprocess.run(cmd, shell=True, check=False, stdout=subprocess.PIPE)
     status = "running" if result.returncode == 0 else "finished"
     return (status, result.stdout.decode())
 
   @staticmethod
   def delete(job_id: str) -> str:
     cmd = f"ps -p {job_id} -o 'pgid'"   # get process group id of {job_id}
-    result = subprocess.run(cmd, check=False, shell=True, capture_output=True)
+    result = subprocess.run(cmd, check=False, shell=True, stdout=subprocess.PIPE)
     if result.returncode != 0:
       return f"process for {job_id} is not found"
     pgid = int(result.stdout.decode().splitlines()[-1])  # process group id
     cmd = f"kill -TERM -{pgid}"
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd, shell=True, check=True)
     return f"process group {pgid} is successfully terminated"
